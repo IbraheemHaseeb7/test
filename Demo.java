2ad70653f79a95ede7e106004d61ba14aa012014
unknown <ibraheemhaseeb7@gmail.com>
2022-10-01 21:39:06 +0500

    adding changes to master for testing purposes

--- Demo.java
+++ Demo.java
@@ -1,5 +1,9 @@
 public class Demo {
     public static void main(String args[]) {
-        System.out.println("Hello World");
+        sayHello();
+    }
+
+    public static void sayHello() {
+        System.out.println("saying hello...");
     }
 }
